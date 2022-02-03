a174f8ffd96d80281c766a2ec6495bd2d27bebb1
alenocot <73299908+alenocot@users.noreply.github.com>
2022-02-03 01:40:40 -0500

    Datos del puesto para mostrar en pantalla

---
+++ frontend/passantic/lib/models/puesto.dart
@@ -0,0 +1,10 @@
+class Puesto {
+  final String cargo;
+  final String imagen;
+  final String descripcion;
+  final double salario;
+  final String modalidad;
+  
+
+  Puesto(this.cargo, this.imagen,this.descripcion,this.salario,this.modalidad);
+}
