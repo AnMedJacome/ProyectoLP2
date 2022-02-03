11f69847713a08495263eb78332b3fbe88ef7560
alenocot <73299908+alenocot@users.noreply.github.com>
2022-02-03 01:01:41 -0500

    Update detalleEmpresa.dart

--- frontend/passantic/lib/ui/views/alejandra/detalleEmpresa.dart
+++ frontend/passantic/lib/ui/views/alejandra/detalleEmpresa.dart
@@ -5,43 +5,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:passantic/constants/colors.dart';
 void main() => runApp(MyApp());
-class AppColors {
-  static MaterialColor mainColor = const MaterialColor(
-    0xFFF3CC44,
-    <int, Color>{
-      50: Color(0xFFF3CC44),
-      100: Color(0xFFF3CC44),
-      200: Color(0xFFF3CC44),
-      300: Color(0xFFF3CC44),
-      400: Color(0xFFF3CC44),
-      500: Color(0xFFF3CC44),
-      600: Color(0xFFF3CC44),
-      700: Color(0xFFF3CC44),
-      800: Color(0xFFF3CC44),
-      900: Color(0xFFF3CC44),
-    },
-  );
-
-  //static const PrimaryColor = Color(0xFF0C4B8D);
-  static const primaryColor = Color(0xFFF3CC44);
-
-  static const Color yellow = Color(0xFFEEC415);
-  static const Color green = Color(0xFF61BF33);
-  static const Color orange = Color(0xFFE96B1A);
-  static const Color red = Color(0xFFDF1919);
-  static const Color background = Colors.white24;
-
-  static const Color textlight = Colors.black87;
-  static const Color textDark = Colors.white;
-
-  // Shimmer Colors
-  static final Color shimmerBackground = Colors.grey.shade300;
-  static final Color shimmerBaseColor = Colors.grey.shade300;
-  static final Color shimmerHighlightColor = Colors.grey.shade100;
-}
-
 
 class MyApp extends StatelessWidget {
   @override
